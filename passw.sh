b486f9a05394205e9f9b1b89bcaa5d60b9e0b151
p31x070 <peixoto@riseup.net>
2020-01-30 17:14:28 -0300

    depois de algumas tentativas encontrei a maneira correta de escrever

--- passw.sh
+++ passw.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-cp /media/peixoto/Documentos/KeepPass/Bancos\ de\ Dados/Lovemenow.kdbx /home/peixoto/Área\ de\ Trabalho\
+#cp /media/peixoto/Documentos/KeepPass/Bancos\ de\ Dados/Lovemenow.kdbx /home/peixoto/Documentos
+##thunderbird --compose "to=luizpeixoto.adv@gmail.com", "subject=Atualização de Senha", "attachment=/home/peixoto/Documentos/Lovemenow.kdbx"
+#
+#
+#to=$"to=luizpeixoto.adv@gmail.com"
+#subject=$"subject=Atualização de Senha"
+#attachment=$"attchment=/peixoto/Documentos/Lovemenow.kdbx"
+#
+#print $attachment
+#
+##thunderbird --compose $to,$subject,$subject,$attachment
+#thunderbird --compose $attachment
+
+att=$"attachment=/media/peixoto/Documentos/KeepPass/Bancos de Dados/Lovemenow.kdbx"
+#msg=$"caminho para o arquivo txt"
+sub=$"subject=Atualização de Senha"
+to=$"to=luizpeixoto.adv@gmail.com"
+
+thunderbird --compose $to,$sub,$att
