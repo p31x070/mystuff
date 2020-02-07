562799a36785591564632ed8611babf3de447a13
p31x070 <peixoto@riseup.net>
2020-02-07 14:19:40 -0300

    Inseri caminho para script do envio da mensagem e chamado para execução do script

---
+++ MessageWhatsapp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Criar um arquivo de mensagem com data atualizada
+# Inserir o cálculo da hora marcada com os fusos horários
+# Copiar para área de trasferência para mensagem
+# Verificar se é possívle atribuir uma variável em um bash
+# para depois recuperar em outro
+# usar o comando curl -L | bash para chamar várias sequencias de scripts
